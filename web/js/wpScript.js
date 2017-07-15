56f4c3127e3a762640eba43a38c7753c3bcc7b17
Jonick <bjonick@gmail.com>
2017-07-15 11:31:32 +0300

    js

---
+++ web/js/wpScript.js
@@ -0,0 +1,13 @@
+
+actionsTableProjects();
+
+function actionsTableProjects() {
+    var projectTableRow = document.querySelectorAll(".table-projects>tbody>tr");
+    for (var i = 0; i < projectTableRow.length; i++) {
+        projectTableRow[i].addEventListener("click", function(){
+            window.location.href = "/web/projects/view?id="+this.getAttribute("data-key");
+        });
+    }
+}
+
+
